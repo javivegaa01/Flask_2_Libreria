419f00f70905eea587aff8343642458a85fcdedc
Javier Vega Correa <javier@debian>
2021-04-30 18:47:03 +0200

    .

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+import os
+import json
 from flask import Flask, render_template,abort
-app = Flask(__name__)
+app = Flask(__name__)	
 
-@app.route('/')
+with open("books.json") as fichero:
+    datos=json.load(fichero)
+
+@app.route('/',methods=["GET","POST"])
 def inicio():
-    return render_template("index.html")
+	return render_template("inicio.html",libros=datos)
 
-app.run(debug=True)
+@app.route('/libro/<isbn>',methods=["GET","POST"])
+def libros(isbn):
+    for libro in datos:
+        if "isbn" in libro.keys() and isbn==libro["isbn"]:
+	        return render_template("libros.html",libro=libro)
+    abort(404)
+
+@app.route('/categoria/<categoria>',methods=["GET","POST"])
+def categoria(categoria):
+    for libro in datos:
+        if "categories" in libro.keys() and categoria in libro["categories"]:
+	        return render_template("categorias.html",libros=datos,categoria=categoria)
+    abort(404)
+
+port=os.environ["PORT"]
+app.run('0.0.0.0',int(port),debug=False)
